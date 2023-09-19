a069d941da8d2722dba224d625e4a1812a73d327
Danny Hogan <dlhogan@uw.edu>
2023-09-19 12:36:58 -0700

    updates to files

--- Final/snow_pit_parser.py
+++ Final/snow_pit_parser.py
@@ -93,14 +93,14 @@ def build_snowpit_ds(fn):
     ds = xr.Dataset(
     data_vars=dict(
         temperature=(['depth'],temps,dict(
-                                        units='C',
-                                        method='thermometer',
+                                        units='degC',
+                                        long_name='Snow pit profile temperature from Cooper-Atkins DFP450W-0-8 4 digital thermometer',
                                         )
                 ),
         density=(['depth'],density,dict(
-                                        units='kg/m3',
-                                        method='density cutter',
-                                        note='10 cm layer average'
+                                        units='kg/m^3',
+                                        long_name='Snow density from Snowmetrics density cutter and scale',
+                                        measurement_notes='Measurements taken from the surface every 10 cm, so density is a 10-cm layer average.'
                                         )
                 )
     ),
@@ -176,6 +176,53 @@ def multi_pit_ds(filenames):
     return data_ds, layer_ds
 ds_data, ds_layer = multi_pit_ds(fns)
 
+# add surface temperatures 
+pit_surf_temp = pd.read_csv('./snow_pit_surface_temps.csv', index_col=0)
+pit_surf_temp.index = ds_data.time.values
+# convert to xarray dataset
+ds_surf_temp = xr.Dataset.from_dataframe(pit_surf_temp)
+# # rename dimension id
+ds_surf_temp = ds_surf_temp.rename({'index': 'time',
+                                    'IR':'ir_surface_temp',
+                                    'Therm':'thermometer_surface_temp'})
+# add a new variable called snow_depth and make in equal to the last depth value in the snowpit and associate it with time
+snow_depth = pd.DataFrame([ds_data.isel(time=i).temperature.dropna(dim='depth').depth.max().values for i in range(len(ds_data.time))], index=ds_data.time.values, )
+# convert to xarray dataset
+snow_depth_ds = xr.Dataset.from_dataframe(snow_depth)
+# # rename dimension id
+ds_snow_depth = snow_depth_ds.rename({'index': 'time',
+                                     0: 'pit_total_snow_depth'})
+# merge into ds_data
+ds_data = xr.merge([ds_data, ds_surf_temp,ds_snow_depth])
+
+
+# iterate to set the first value in the temperature variable to thermometer_surface_temp
+for i in range(len(ds_data.time)):
+    ds_data['temperature'].values[i][0] = ds_data['thermometer_surface_temp'].values[i]
+# add attribute to each ir_surface_temp
+ds_data['ir_surface_temp'].attrs['units'] = 'degC'
+ds_data['ir_surface_temp'].attrs['long_name'] = 'Infrared surface temperature from IR thermometer gun'
+
+# add attribute to each thermometer_surface_temp
+ds_data['thermometer_surface_temp'].attrs['units'] = 'degC'
+ds_data['thermometer_surface_temp'].attrs['long_name'] = 'Surface temperature from Cooper-Atkins DFP450W-0-8 4 digital thermometer'
+
+# add attribute to each pit_total_snow_depth
+ds_data['pit_total_snow_depth'].attrs['units'] = 'cm'
+ds_data['pit_total_snow_depth'].attrs['long_name'] = 'Total snow depth from snow pit'
+
+# convert time to utc
+ds_data["time"] = pd.to_datetime(ds_data.time.values).tz_localize('MST').tz_convert('UTC').astype('datetime64[ns]')
+# rename datetime_str attribute to local_time_MST
+ds_data.attrs['local_time'] = 'MST (UTC-7)'
+# add attribute about time
+ds_data.attrs['time_info'] = 'Start time of snow pit measurements (beginning with surface temperature)'
+ds_data.attrs['timezone_info'] = 'Data presented in UTC'
+
+# Save to netcdf
+if __name__ == "__main__":
+    ds_data.to_netcdf("./kettle_ponds_snowpit_profiles.nc")
+    ds_layer.to_netcdf("./kettle_ponds_snowpit_layers.nc")
 if not os.path.exists("./kettle_ponds_snowpit_profiles.nc"):
     ds_data.to_netcdf("./kettle_ponds_snowpit_profiles.nc")
     ds_layer.to_netcdf("./kettle_ponds_snowpit_layers.nc")
